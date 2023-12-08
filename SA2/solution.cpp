a9779e6eadbd614bdd9308b80fae1726f1d6374d
rishavkrd <rishavkrd11@tamu.edu>
2023-12-08 17:04:22 -0600

    [SA_2] Test Script implementaton and starter code

--- SA2/solution.cpp
+++ SA2/solution.cpp
@@ -20,35 +20,11 @@ Node* createTree(){
     Node* root = new Node(1);
     root->left = new Node(2);
     root->right = new Node(3);
-    root->right->left = new Node(4);
-    root->right->right = new Node(5);
-    root->right->right->left = new Node(6);
-    root->right->right->right = new Node(7);
     return root;
 }
 
 int consecutiveParentChild(Node* root){
-    if(root == NULL){
-        return 0;
-    }
-    int leftCount = 0, rightCount = 0;
-    if(root->left){
-        if(root->left->data == root->data + 1 || root->left->data == root->data - 1){
-            cout<<root->data<<","<<root->left->data<<endl;
-            leftCount = 1 + consecutiveParentChild(root->left);
-        } else{
-            leftCount = consecutiveParentChild(root->left);
-        }
-    }
-    if(root->right){
-        if(root->right->data == root->data + 1 || root->right->data == root->data - 1){
-            cout<<root->data<<","<<root->right->data<<endl;
-            rightCount = 1 + consecutiveParentChild(root->right);
-        } else{
-            rightCount = consecutiveParentChild(root->right);
-        }
-    }
-    return leftCount + rightCount;
+
 
 }
 
@@ -60,44 +36,7 @@ void testConsecutiveParentChild(){
 }
 
 vector<int> reverseLevelOrder(Node* root){
-    queue<Node*> q;
-    stack<Node*> s;
-
-    q.push(root);
-    q.push(NULL);
-
-    while(!q.empty()){
-        Node* current = q.front();
-        q.pop();
-        if(current == NULL){
-            if(!q.empty()){
-                q.push(NULL);
-                s.push(NULL);
-            }
-            continue;
-        }
-        s.push(current);
-
-        if(current->right){
-            q.push(current->right);
-        }
-        if(current->left){
-            q.push(current->left);
-        }
-
-    }
-    vector<int> result;
-    while(!s.empty()){
-        if(s.top() == NULL){
-            cout <<endl;
-            s.pop();
-            continue;
-        }
-        result.push_back(s.top()->data);
-        cout<<s.top()->data<<", ";
-        s.pop();
-    }
-    return result;
+    
 }
 
 void testReverseLevelOrder(){
@@ -110,9 +49,10 @@ void testReverseLevelOrder(){
     }
     cout<<endl;
 }
-
+#ifndef TEST
 int main(){
     testConsecutiveParentChild();
     testReverseLevelOrder();
     return 0;
 }
+#endif
