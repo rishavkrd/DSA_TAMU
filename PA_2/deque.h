8b12b287c6e7bd53be32a1e44c6c80be82f70287
rishavkrd <rishavkrd11@tamu.edu>
2023-12-08 15:53:12 -0600

    [LE_5, LE_6, LE_7, LE_8, PA_1, PA_2] Test Script implementaton and starter code

--- PA_2/deque.h
+++ PA_2/deque.h
@@ -10,9 +10,6 @@ private:
 
 public:
 	Deque() {
-        s=0;
-		firstNode = nullptr;
-		lastNode = nullptr;
 	}
 
 	~Deque() {
@@ -21,72 +18,38 @@ public:
 	}
 	
 	bool isEmpty() {
-        return s==0;
 	}
 
 	int size() {
-        return s;
 	}
 
 	Type first() {
-        return firstNode->getData();
+
 	}
 
 	Type last() {
-        return lastNode->getData();
+
 	}
 
 	void insertFirst(Type o) {
-        Node<Type>* newNode = new Node<Type>(o);
-		if(isEmpty()){
-			firstNode = newNode;
-			lastNode = newNode;
-		}
-		else{
-			newNode->setNext(firstNode);
-			firstNode->setPrev(newNode);
-			firstNode = newNode;
-		}
-		s++;
+
 	}
 
 	void insertLast(Type o) {
-        Node<Type>* newNode = new Node<Type>(o);
-		if(isEmpty()){
-			firstNode = newNode;
-			lastNode = newNode;
-		}
-		else{
-			newNode->setPrev(lastNode);
-			lastNode->setNext(newNode);
-			lastNode = newNode;
-		}
-		s++;
+
 	}
 	
 	Type removeFirst() {
-        if(isEmpty()){
-			cout<<"Deque is empty"<<endl;
-			return Type();
-		} else{
-			Node<Type>* temp = firstNode;
-			firstNode = firstNode->getNext();
-			if(firstNode!=NULL) firstNode->setPrev(nullptr);
-			s--;
-			return temp->getData();
-		}
+
 	}
 
 	Type removeLast() {
-        if(isEmpty()){
-			cout<<"Deque is empty"<<endl;
-			return Type();
-		} else{
-			Node<Type>* temp = lastNode;
-			lastNode = lastNode->getPrev();
-			if(lastNode!=NULL) lastNode->setNext(nullptr);
-			s--;
-			return temp->getData();
-		}
+
 	}
 };
+
+#ifndef TEST
+int main(){
+	return 0;
+}
+#endif
--- PA_2/main.cpp
+++ PA_2/main.cpp
@@ -5,9 +5,8 @@
 #include "deque.h"
 
 using namespace std;
-
-int main()
-{
+#ifndef TEST
+int main(){
 	Deque<int> intQ;
 	assert(intQ.size() == 0);
 	intQ.insertFirst(1);
@@ -27,130 +26,6 @@ int main()
 	assert(intQ.size() == 2);
 	assert(intQ.removeLast() == 1);
 	assert(intQ.removeLast() == 0);
-
-    cout<<"test3"<<endl;
-	assert(intQ.size() == 0);
-	intQ.insertFirst(1);
-	intQ.insertFirst(0);
-	assert(intQ.removeFirst() == 0);
-	assert(intQ.removeFirst() == 1);
-
-	assert(intQ.size() == 0);
-	intQ.insertFirst(1);
-	intQ.insertFirst(0);
-	assert(intQ.removeFirst() == 0);
-	assert(intQ.removeLast() == 1);
-    cout<<"test4"<<endl;
-	assert(intQ.size() == 0);
-	intQ.insertFirst(1);
-	intQ.insertFirst(0);
-	assert(intQ.removeLast() == 1);
-	assert(intQ.removeFirst() == 0);
-
-	assert(intQ.size() == 0);
-	intQ.insertLast(1);
-	assert(intQ.size() == 1);
-	assert(intQ.removeFirst() == 1);
-
-	assert(intQ.size() == 0);
-	intQ.insertLast(1);
-	assert(intQ.size() == 1);
-	assert(intQ.removeLast() == 1);
-    cout<<"test5"<<endl;
-	assert(intQ.size() == 0);
-	intQ.insertLast(0);
-	intQ.insertLast(1);
-	assert(intQ.size() == 2);
-	assert(intQ.removeLast() == 1);
-	assert(intQ.removeLast() == 0);
-
-	assert(intQ.size() == 0);
-	intQ.insertLast(0);
-	intQ.insertLast(1);
-	assert(intQ.removeFirst() == 0);
-	assert(intQ.removeFirst() == 1);
-
-	assert(intQ.size() == 0);
-	intQ.insertLast(0);
-	intQ.insertLast(1);
-	assert(intQ.removeFirst() == 0);
-	assert(intQ.removeLast() == 1);
-    cout<<"test6"<<endl;
-	assert(intQ.size() == 0);
-	intQ.insertLast(0);
-	intQ.insertLast(1);
-	assert(intQ.removeLast() == 1);
-	assert(intQ.removeFirst() == 0);
-
-	assert(intQ.size() == 0);
-	intQ.insertFirst(1);
-	intQ.insertFirst(0);
-	intQ.insertLast(2);
-	intQ.insertLast(3);
-	intQ.insertLast(4);
-	intQ.insertLast(5);
-	intQ.insertLast(6);
-    cout<<"test7"<<endl;
-	for (int i = 0; i < 7; i++) {
-		assert(intQ.removeFirst() == i);
-		assert(intQ.size() == 6 - i);
-	}
-
-	assert(intQ.size() == 0);
-    cout<<"test8"<<endl;
-	intQ.insertFirst(1);
-	intQ.insertFirst(0);
-	intQ.insertLast(2);
-	intQ.insertLast(3);
-	intQ.insertLast(4);
-	intQ.insertLast(5);
-	intQ.insertLast(6);
-	for (int i = 0; i < 7; i++) {
-		assert(intQ.removeLast() == 6 - i);
-		assert(intQ.size() == 6 - i);
-	}
-
-	assert(intQ.size() == 0);
-	intQ.insertLast(2);
-	intQ.insertLast(3);
-	intQ.insertLast(4);
-	intQ.insertLast(5);
-	intQ.insertLast(6);
-	intQ.insertFirst(1);
-	intQ.insertFirst(0);
-	for (int i = 0; i < 7; i++) {
-		assert(intQ.removeLast() == 6 - i);
-		assert(intQ.size() == 6 - i);
-	}
-    cout<<"test9"<<endl;
-	assert(intQ.size() == 0);
-	intQ.insertLast(2);
-	intQ.insertLast(3);
-	intQ.insertLast(4);
-	intQ.insertLast(5);
-	intQ.insertLast(6);
-	intQ.insertFirst(1);
-	intQ.insertFirst(0);
-	for (int i = 0; i < 7; i++) {
-		assert(intQ.removeFirst() == i);
-		assert(intQ.size() == 6 - i);
-	}
-
-	assert(intQ.isEmpty());
-	intQ.insertFirst(1);
-	assert(intQ.first() == 1);
-	assert(intQ.last() == 1);
-	assert(!intQ.isEmpty());
-	assert(intQ.removeLast() == 1);
-    cout<<"test10"<<endl;
-	assert(intQ.isEmpty());
-	intQ.insertLast(1);
-	assert(intQ.first() == 1);
-	assert(intQ.last() == 1);
-	assert(!intQ.isEmpty());
-	assert(intQ.removeFirst() == 1);
-
-	cout << "All tests passed." << endl;
-
 	return 0;
 }
+#endif
