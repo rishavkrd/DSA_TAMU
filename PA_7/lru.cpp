5ee54dd509adc8f5209f215624414d4654b9f0cb
rishavkrd <rishavkrd11@tamu.edu>
2023-12-13 01:40:14 -0600

    [PA_7] LRU Cache Question

--- PA_7/lru.cpp
+++ PA_7/lru.cpp
@@ -1,3 +1,11 @@
+//Least Recently Used Cache Implementation
+// The size of the Cache is limited to maxSize. If the cache is full and we want to insert a new item, we remove the least recently used item from the cache.
+// Implement with O(1) time complexity the following functions:
+    // 1. insertKeyValue(string key, int value) - insert a key value pair into the cache. If the key already exists, update the value of the key.
+    // 2. getValue(string key) - return the value of the key if it exists, otherwise return NULL.
+    // 3. mostRecentKey() - return the key of the most recently used value.
+
+
 #include<iostream>
 #include<list>
 #include<unordered_map>
